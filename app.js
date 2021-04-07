3b061b8bdaf67a7732b22c281fb8657d9003c7cb
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-04-07 13:34:27 -0400

    add auth middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -18,14 +24,27 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
